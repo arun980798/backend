7e9d4ded81228fbdb95ae9e1adeb7a409fb60606
arun980798 <arunrajput88176894@gmail.com>
2026-01-26 21:44:45 +0530

    day 2 1st server

---
+++ 2day/server.js
@@ -0,0 +1,18 @@
+const express = require('express'); //yaha se server inport karta h 
+
+
+const app = express(); //ye  servver ko banata h 
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, o batao  World!');
+})// this is the api which work  on the "/" 
+
+app.get('/appo', (req, res) => {
+    res.send('hii , o batao  World!');
+})// this is the api which work  on the "/" 
+let aoo  = 0;
+app.listen(3000, () => {
+aoo++;
+    console.log('Server is running on port 3000');
+});// ye  app ko port det
